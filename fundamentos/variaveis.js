5d2a818fa9f72b35d2fb0512c12f7fd643bd766c
Kimberlysugai <sugaisilvawagner@gmail.com>
2024-09-13 21:35:38 -0300

    estado das variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,24 @@
+/** 
+ * Estudo do constante e variáveis
+ * @author Kimberly Sugai
+ */
+  
+ console.clear()
+
+ //variaveis
+ let nome
+ let idade
+ let vip
+
+ nome = "Kimberly Sugai"
+ console.log(typeof(nome))
+ console.log(nome)
+
+
+ idade = 28
+ console.log(typeof(idade))
+ console.log(idade)
+
+ vip = trueconsole.log(typeof(vip))
+ console.log(vip)
+ 
